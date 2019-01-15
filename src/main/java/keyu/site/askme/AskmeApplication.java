39b178f5d5b84cc798b1528d6c5965bd5f0d93b4
Brabbit <347560833@qq.com>
2019-01-15 13:14:11 +0800

    Spring Boot初始环境

---
+++ src/main/java/keyu/site/askme/AskmeApplication.java
@@ -0,0 +1,14 @@
+package keyu.site.askme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AskmeApplication.class, args);
+    }
+
+}
+
